a117192ebfd6d37bbc35adf9037dbc047c0c19a0
Fredrik Stetler <fredrik.stetler@cytiva.com>
2026-02-18 10:06:15 +0100

    fixing github

--- MyColabProject/MyColabProject/Program.cs
+++ MyColabProject/MyColabProject/Program.cs
@@ -3,5 +3,6 @@
     public static void Main(string[] args)
     {
         Console.WriteLine("Hello, Arvid!");
+        Console.WriteLine("This is my first update to the project, hope github works properly");
     }
 }
