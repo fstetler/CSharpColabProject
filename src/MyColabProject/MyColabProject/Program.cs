faaa48c51f6b20a275a9b0abd481487999f851fe
Fredrik Stetler <fredrik.stetler@cytiva.com>
2026-02-18 10:15:27 +0100

    .\MyColabProject\Program.cs

--- src/MyColabProject/MyColabProject/Program.cs
+++ src/MyColabProject/MyColabProject/Program.cs
@@ -5,5 +5,6 @@
         Console.WriteLine("Hello, Arvid!");
         Console.WriteLine("This is my first update to the project, hope github works properly");
         Console.WriteLine("Testing to write a line of code");
+        Console.WriteLine("Testing lastly to see if everything is good");
     }
 }
