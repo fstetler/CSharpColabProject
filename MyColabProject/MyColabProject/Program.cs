fa0a6131fd622ac8c65048685e63478e6a1dcacd
Fredrik Stetler <fredrik.stetler@cytiva.com>
2026-02-18 10:11:54 +0100

    fixing gitignore

--- MyColabProject/MyColabProject/Program.cs
+++ MyColabProject/MyColabProject/Program.cs
@@ -4,5 +4,6 @@
     {
         Console.WriteLine("Hello, Arvid!");
         Console.WriteLine("This is my first update to the project, hope github works properly");
+        Console.WriteLine("Testing to write a line of code");
     }
 }
