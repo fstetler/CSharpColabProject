00f1a4beac5ab4f4026830a67e767115b9852ff3
Fredrik <fredrikstetler@gmail.com>
2026-02-20 15:16:32 +0100

    added ability ability to read mediatrkey txt file

--- MyColabApiProject/MyColabApiProject/Program.cs
+++ MyColabApiProject/MyColabApiProject/Program.cs
@@ -1,16 +1,22 @@
 using Microsoft.EntityFrameworkCore;
 using MyColabApiProject;
 using MediatR;
+using System.IO;
 
 public class Program
 {
     public static void Main(string[] args)
     {
 
-        string licenseKey = 
-            "eyJhbGciOiJSUzI1NiIsImtpZCI6Ikx1Y2t5UGVubnlTb2Z0d2FyZUxpY2Vuc2VLZXkvYmJiMTNhY2I1OTkwNGQ4OWI0Y2IxYzg1ZjA4OGNjZjkiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2x1Y2t5cGVubnlzb2Z0d2FyZS5jb20iLCJhdWQiOiJMdWNreVBlbm55U29mdHdhcmUiLCJleHAiOiIxODAzMDgxNjAwIiwiaWF0IjoiMTc3MTU5NTIzNyIsImFjY291bnRfaWQiOiIwMTljN2I0ZGMzNjg3NzljYjEwMmY3NWQ4NWFjYTUyNSIsImN1c3RvbWVyX2lkIjoiY3RtXzAxa2h4bXcyNW5hN21nZms4OXRiYnlkMHpzIiwic3ViX2lkIjoiLSIsImVkaXRpb24iOiIwIiwidHlwZSI6IjIifQ.mgJcNobOPS9J7NfJUheDFZuzj2vbGQQ60zjjZshS74Z9Oqi7gjAxFbUAkG3K5SVRJ8X3Bm-fp-IXkh8eODhr0ejwph1kniRXae6cwfp-ddtNbefJZaW36BpxCGjfPgNPYGAjeUTxIERJ_SiM8ugrcVuEEsQ7gDTfXVau3eYjk_CkLQZZFaW86aX1V7gLmv4LbMwdY0zih9PHfnq2ITgC8Gqhhbs78XuXx6cX6zyPB4zVYRnG0NoOM-R0ZzTZwBcYj_3Hp7r0h7NnovWIP7NGYHGN4OeL9fDtNy-JBpaF_RFEGqGKqdYLjUJSdx2AQ7SMSDczjB4bM6Q_pvuLtRF5aQ";
-
         var builder = WebApplication.CreateBuilder(args);
+
+        var keyFilePath = Path.Combine(builder.Environment.ContentRootPath, "Resources", "MediatRKey.txt");
+        if (!File.Exists(keyFilePath))
+        {
+            throw new FileNotFoundException($"MediatR license key file not found: {keyFilePath}");
+        }
+
+        var licenseKey = File.ReadAllText(keyFilePath).Trim();
 
         builder.Services.AddDbContext<PersonDbContext>(
             options =>
