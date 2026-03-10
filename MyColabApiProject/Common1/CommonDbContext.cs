11c40a22de86afd9789941d91c520afcec08cb84
Fredrik Stetler <fredrik.stetler@cytiva.com>
2026-03-10 15:39:24 +0100

    returned common1

---
+++ MyColabApiProject/Common1/CommonDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Common
+{
+    public class CommonDbContext<TEntity> : DbContext where TEntity : class
+    {
+
+        public CommonDbContext(DbContextOptions<CommonDbContext<TEntity>> options) : base(options)
+        {
+        }
+
+        public DbSet<TEntity> GetAll { get; set; }
+
+    }
+}
---
+++ MyColabApiProject/Common1/RepositoryBase.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Nest;
+
+namespace Common
+{
+    public class RepositoryBase<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+
+        private readonly CommonDbContext<TEntity> _dB;
+
+        public RepositoryBase(CommonDbContext<TEntity> dbContext)
+        {
+            _dB = dbContext;
+        }
+
+        public Task<List<TEntity>> GetAllAsync()
+        {
+            return _dB.GetAll.ToListAsync();
+        }
+    }
+}
