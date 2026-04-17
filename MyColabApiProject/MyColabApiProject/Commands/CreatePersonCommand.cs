a4566b1c12f46300628ec0d6df0bfdf16bf7a995
Fredrik <fredrikstetler@gmail.com>
2026-04-17 15:39:38 +0200

    Removed ID from CreatePersonCommand

--- MyColabApiProject/MyColabApiProject/Commands/CreatePersonCommand.cs
+++ MyColabApiProject/MyColabApiProject/Commands/CreatePersonCommand.cs
@@ -1,13 +1,10 @@
 ﻿using Common.CommonCommands;
 using MyColabApiProject.Domains;
-using System.Text.Json.Serialization;
 
 namespace MyColabApiProject.Commands
 {
     public class CreatePersonCommand : CommandBase<PersonDto>
     {
-        [JsonIgnore]
-        public Guid Id { get; set; }
         public required String Name { get; set; }
     }
 }
